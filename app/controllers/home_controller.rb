fbdb51266e15b78a6291c790e758386deb2c720a
qufehdquf9 <qufehdquf9@naver.com>
2018-05-28 08:40:20 +0000

    add comment

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,44 @@
+class HomeController < ApplicationController
+  #protect_from_forgery with: :exception
+  def index
+    @posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    @post=Post.new
+    @post.title = params[:post_title]
+    @post.content = params[:post_content]
+    @post.save
+    
+    redirect_to '/home/index'
+  end
+  
+  def delete
+    @post = Post.find(params[:post_id])
+    @post.destroy
+    
+    redirect_to '/'
+  end
+  def edit
+    @post = Post.find(params[:post_id])
+  end
+  
+  def update
+    @post = Post.find(params[:post_id])
+    @post.title = params[:post_title]
+    @post.content = params[:post_content]
+    @post.save
+    
+    redirect_to '/'
+  end
+  
+  def comment_create
+    comment = Post.find(params[:p_id]).comments.create(content: params[:content])
+    comment.save
+    
+    redirect_to '/'
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  root 'home#index'
+  
+  get 'home/index'
+
+  get 'home/new'
+
+  post 'home/create'
+  
+  get 'home/delete/:post_id' => 'home#delete'
+
+  get 'home/edit/:post_id' => 'home#edit'
+  
+  post 'home/update/:post_id' => 'home#update'
+  
+  post '/:p_id/comment_create' =>'home#comment_create'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
